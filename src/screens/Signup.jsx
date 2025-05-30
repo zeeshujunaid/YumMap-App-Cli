3ebebe9dcd10ff09f6560bc9853070f76960d212
ZEESHAN JUNAID <zeeshan.junaid.developer@gmail.com>
2025-05-30 21:28:40 +0500

    Update Signup.jsx

--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -5,7 +5,7 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 export default function SignupScreen({navigation}) {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Signup Screen</Text>
+            <Text style={styles.title}>SIGNUP SCREEN FOR RES AND USER BOTH </Text>
             <Button
                 title="Go to Home"
                 onPress={() => navigation.navigate('MainApp')}
